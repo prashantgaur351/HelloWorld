54aa84aae469f2a6e5afc1e14e6d7b9e4af870b3
PrashantGaur <prashantgaur351@gmail.com>
2022-01-22 18:05:03 +0530

    Factorail Program

---
+++ abc.java
@@ -0,0 +1,30 @@
+class abc
+{
+public static void main(String arg[])
+{
+int fact=1,num=5;
+for(int i=1;i<=num;i++)
+{
+fact=fact*i;
+}
+System.out.println(fact);
+}
+}
+
+/**
+Dear All,
+
+Today is my last working day at Cargoflash.
+
+Finally the moment has arrived to say goodbye, which is never easy, especially when you have been part of the Cargoflash family for more than a years .It has been a fantastic journey and a great learning experience.
+The people here at Cargoflash have honestly been one of the best parts of working here.
+
+Hearty Thanks to all my colleagues [Rishabh, Sandhya, Trilok, Shubham, Uttam, Nehal, Ankit, Mukesh] and all support team members who were always there for me.
+Sincere special thanks to my Seniors  "Umar Sir", "Devender Sir", "Parvez Sir".
+
+
+Thanks & Best Regards,
+Prashant Gaur
+M: +919520578825
+
+**/
